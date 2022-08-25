2643586f3b968d720213b437f610e6ec7b651c51
Dipesh Rai <raidipesh78@gmail.com>
2022-08-25 19:47:45 +0545

    feat: port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import path from 'path'
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: 'REACT_APP_',
   plugins: [react()],
   resolve: {
     alias: {
       src: path.join(__dirname, 'src')
     }
-  }
+  },
+  server: {port: 3000}
 })
