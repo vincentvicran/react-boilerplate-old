b5f99c7525b9cd694e832ca85f91dc1154771b79
Dipesh Rai <raidipesh78@gmail.com>
2022-09-02 11:39:11 +0545

    fix: null return on header if not logged

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -3,12 +3,14 @@ import {useAuth} from 'react-auth-navigation'
 export const Header = () => {
   const auth = useAuth()
 
-  return auth.isLoggedIn ? (
+  if (!auth.isLoggedIn) {
+    return null
+  }
+
+  return (
     <div>
       <h1>Heading</h1>
       <button onClick={() => auth.logout()}>Logout</button>
     </div>
-  ) : (
-    <></>
   )
 }
