aabfcbb5f0cc666cdaddd7517449e9f7aa77a398
Dipesh Rai <raidipesh78@gmail.com>
2022-09-13 10:34:50 +0545

    feat: added formik and removed use form input;

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,5 +1,6 @@
 import {useAuth} from 'react-auth-navigation'
-import {useFormInput} from 'use-form-input'
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
 
 import {useDispatch, useSelector} from 'src/store'
 
@@ -10,43 +11,54 @@ export const Login = () => {
   const dispatch = useDispatch()
   const {loading} = useSelector((state) => state.login)
 
-  const [data, {onChange, onSubmit}] = useFormInput(
-    {
-      email: '',
-      password: ''
-    },
-    (values) => {
-      if (values.email && values.password) {
-        dispatch(
-          login({
-            email: values.email,
-            password: values.password,
-            onSuccess: () => auth.login()
-          })
-        )
-      }
+  const formik = useFormik<{email: string; password: string}>({
+    initialValues: {email: '', password: ''},
+    validationSchema: Yup.object({
+      email: Yup.string().email().required('Email is required'),
+      password: Yup.string().min(8).required('Password is required')
+    }),
+    onSubmit: (values) => {
+      dispatch(
+        login({
+          email: values.email,
+          password: values.password,
+          onSuccess: () => auth.login()
+        })
+      )
     }
-  )
+  })
 
   return (
     <div>
       <h3>Login</h3>
 
-      <form onSubmit={onSubmit}>
-        <input
-          type="email"
-          placeholder="Email address"
-          name="email"
-          value={data.email}
-          onChange={onChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={data.password}
-          onChange={onChange}
-        />
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          <div style={{color: 'red', fontSize: 12}}>
+            {formik.touched.email && formik.errors.email}
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          <div style={{color: 'red', fontSize: 12}}>
+            {formik.touched.password && formik.errors.password}
+          </div>
+        </div>
+
         {loading ? (
           <span>Loading...</span>
         ) : (
