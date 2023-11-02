0f4f94fa7373e43c140e6538e8d04b6282738128
vincentvicran <vikrant6157@gmail.com>
2023-11-02 11:28:15 +0545

    fix: errors

--- src/app/pages/sample/sample.page.tsx
+++ src/app/pages/sample/sample.page.tsx
@@ -1,11 +1,8 @@
-import {toast} from 'react-hot-toast'
-import {Button, ToolTip} from 'src/app/common'
-import {Dropdown} from 'src/app/common'
-import {Menu} from 'src/app/common'
-import {HStack, VStack, ResponsiveStack} from 'src/app/common'
-import {Table} from 'src/app/common'
-import {Skeleton} from 'src/app/common'
-import {Title} from 'src/app/common/title/title.common'
+import { toast } from 'react-hot-toast'
+import { Button, ToolTip } from 'src/app/common'
+import { HStack, VStack, ResponsiveStack } from 'src/app/common'
+import { Skeleton } from 'src/app/common'
+import { Title } from 'src/app/common/title/title.common'
 // import {CompWrapper} from 'src/app/components'
 // import {TextEditor} from 'src/common/textEditor'
 
@@ -24,7 +21,6 @@ export const Sample = () => {
       <Title smallheading>Small Heading</Title>
 
       <h2>Table</h2>
-      <TableComponent />
 
       <h2>Tooltip</h2>
       <HStack gap="$2" wrap>
@@ -52,91 +48,34 @@ export const Sample = () => {
 
       <h2>Dropdown</h2>
 
-      <Dropdown
-        triggerToggle
-        trigger={() => <Button>Open Custom Dropdown</Button>}
-      >
-        <Menu.Container>
-          <Menu.Item onClick={() => false}>First Element</Menu.Item>
-          <Menu.Item onClick={() => false}>Second Element</Menu.Item>
-          <Menu.Separator />
-          <Menu.Item onClick={() => false} danger>
-            Third Element
-          </Menu.Item>
-        </Menu.Container>
-      </Dropdown>
-
       <h2>HStack</h2>
 
-      <HStack style={{backgroundColor: 'yellow'}}>
-        <div style={{background: 'red', color: 'white'}}>HStack Item 1</div>
-        <div style={{background: 'blue', color: 'white'}}>HStack Item 2</div>
-        <div style={{background: 'green', color: 'white'}}>HStack Item 3</div>
+      <HStack style={{ backgroundColor: 'yellow' }}>
+        <div style={{ background: 'red', color: 'white' }}>HStack Item 1</div>
+        <div style={{ background: 'blue', color: 'white' }}>HStack Item 2</div>
+        <div style={{ background: 'green', color: 'white' }}>HStack Item 3</div>
       </HStack>
 
       <h2>VStack</h2>
-      <VStack style={{backgroundColor: 'orange'}}>
-        <div style={{background: 'red', color: 'white'}}>VStack Item 1</div>
-        <div style={{background: 'blue', color: 'white'}}>VStack Item 2</div>
-        <div style={{background: 'green', color: 'white'}}>VStack Item 3</div>
+      <VStack style={{ backgroundColor: 'orange' }}>
+        <div style={{ background: 'red', color: 'white' }}>VStack Item 1</div>
+        <div style={{ background: 'blue', color: 'white' }}>VStack Item 2</div>
+        <div style={{ background: 'green', color: 'white' }}>VStack Item 3</div>
       </VStack>
 
       <ResponsiveStack
-        style={{backgroundColor: 'yellow', padding: 2}}
+        style={{ backgroundColor: 'yellow', padding: 2 }}
         gap="$0_5"
       >
-        <div style={{flex: 1, height: 200, backgroundColor: 'green'}}>
+        <div style={{ flex: 1, height: 200, backgroundColor: 'green' }}>
           HStack Item 1
         </div>
-        <div style={{flex: 1, height: 200, backgroundColor: 'red'}}>
+        <div style={{ flex: 1, height: 200, backgroundColor: 'red' }}>
           HStack Item 2
         </div>
       </ResponsiveStack>
 
       <h2>Tabs</h2>
     </>
-  )
-}
-
-const TableComponent = () => {
-  return (
-    <Table
-      columns={[
-        {
-          field: 'name',
-          name: 'Name'
-        },
-        {
-          field: 'age',
-          name: 'Age',
-          render: (data) => <div>{data}</div>
-        },
-        {
-          field: 'address',
-          name: 'Address',
-          render: ({line1}) => <div>Address: {line1}</div>
-        }
-      ]}
-      data={Array(5)
-        .fill({
-          name: 'Dipesh',
-          age: 100,
-          address: {
-            line1: 'abc',
-            line2: 'cde'
-          }
-        })
-        .map((d) => d)}
-      loading={false}
-      actions={{
-        onEdit: (data) => {},
-        onView: (data) => {},
-        onDelete: (data) => {}
-      }}
-      pagination={{
-        perPage: 5,
-        totalCount: 20
-      }}
-    />
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
 import ReactDOM from 'react-dom/client'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { Toaster } from 'react-hot-toast'
+import { HashRouter, useRoutes } from 'react-router-dom'
 
-import {store} from 'src/store'
+import { store } from 'src/store'
 import './sass/main.scss'
 import 'react-loading-skeleton/dist/skeleton.css'
-import {HashRouter, useRoutes} from 'react-router-dom'
-import {AuthProvider} from './app/routing'
-import {Router} from './app/routing/routes'
-import {Toaster} from 'react-hot-toast'
-import {Footer, Header, VStack} from './app/common'
+
+import { AuthProvider } from './app/routing'
+import { Router } from './app/routing/routes'
+import { Footer, VStack } from './app/common'
 
 const MemoChild = () => {
   return (
     <AuthProvider>
-      <Header />
-      <VStack justify="space-between" style={{minHeight: 'calc(100vh - 55px'}}>
+      <VStack
+        justify="space-between"
+        style={{ minHeight: 'calc(100vh - 55px' }}
+      >
         <App />
         <Footer />
       </VStack>
@@ -36,5 +39,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
       <MemoChild />
     </Provider>
-  </HashRouter>
+  </HashRouter>,
 )
